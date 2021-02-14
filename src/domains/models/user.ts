cb30705768243018035407f785d5cb5a8e781bb4
masimacbook <masi19bw@gmail.com>
2021-02-14 15:08:35 +0900

    Implements login feature

---
+++ src/domains/models/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+  id: string;
+  mailAddress: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+  lastLoggedinAt: string;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,36 +6,39 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import Header from '../templates/Header';
 import Footer from '../templates/Footer';
 import './pagination.css';
-import '../../styles/sns-icon.css'
+import '../../styles/sns-icon.css';
+import { LoginProvider } from './login';
 
 const queryClient = new QueryClient();
 
 export default ({ Component, pageProps }: AppProps): JSX.Element => (
-  <QueryClientProvider client={queryClient}>
-    <Head>
-      <title>めしまし</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta property="og:site_name" content="めしまし" />
-      <meta property="og:locale" content="ja_JP" />
-      <meta name="description" content="サーバーサイドエンジニアの雑記" />
-      <meta property="og:title" content="めしまし" />
-      <meta
-        property="og:description"
-        content="サーバーサイドエンジニアの雑記"
-      />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://mesimasi.com" />
-      <meta property="og:image" content="/logo.png" />
-      <meta property="og:site_name" content="めしまし" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@masibw" />
-      <meta property="og:locale" content="ja_JP" />
-    </Head>
-    <Header />
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} className="bg-gray" />
-    <Footer />
-  </QueryClientProvider>
+  <LoginProvider>
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>めしまし</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:site_name" content="めしまし" />
+        <meta property="og:locale" content="ja_JP" />
+        <meta name="description" content="サーバーサイドエンジニアの雑記" />
+        <meta property="og:title" content="めしまし" />
+        <meta
+          property="og:description"
+          content="サーバーサイドエンジニアの雑記"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mesimasi.com" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:site_name" content="めしまし" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@masibw" />
+        <meta property="og:locale" content="ja_JP" />
+      </Head>
+      <Header />
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} className="bg-gray" />
+      <Footer />
+    </QueryClientProvider>
+  </LoginProvider>
 );
