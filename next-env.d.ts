b2c60c0ec5d3d86fa3720fcbb583da7322a11ad8
masimacbook <masi19bw@gmail.com>
2021-02-21 00:24:22 +0900

    Add google analytics

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+interface Window {
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string });
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+        // eslint-disable-next-line camelcase
+      event_label: string;
+        // eslint-disable-next-line camelcase
+      event_category: string;
+      value?: number;
+    },
+  );
+}
